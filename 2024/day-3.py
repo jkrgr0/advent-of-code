493baaf3ade59b8acef49fdd4fa2b55cfc329fc0
Janek Kröger <6506725+jkrgr0@users.noreply.github.com>
2024-12-04 07:20:49 +0000

    feat: :sparkles: Rewritten day 3 solutions

--- 2024/day-3.py
+++ 2024/day-3.py
@@ -2,52 +2,49 @@
 """Day 3. Mull It Over."""
 
 import re
+from functools import reduce
+from operator import mul
 
 from aocd import get_data
 
 puzzle_input = get_data(day=3, year=2024)
+
+
+def prod(val_tuple: tuple[int]) -> int:
+    """Calculate the product of all integers in a tuple.
+
+    Args:
+        val_tuple (tuple[int]): A tuple containing integer values.
+
+    Returns:
+        int: The product of all integers in the tuple.
+
+    """
+    int_tuple = tuple(int(val) for val in val_tuple)
+    return reduce(mul, int_tuple, 1)
+
 
 print("# ---- Part 1 ----")
 
 sum_mul: int = 0
 MUL_RE = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
 for line in puzzle_input.splitlines():
-    matches = MUL_RE.findall(line)
-    for match in matches:
-        mul_result = int(match[0]) * int(match[1])
-        sum_mul += mul_result
+    sum_mul += sum([prod(instr) for instr in MUL_RE.findall(line)])
 
 print(f"Sum of all valid mul instructions: {sum_mul}")
 
 print("# ---- Part 2 ----")
 
-
-def prod(val_tuple: tuple) -> int:
-    res = 1
-    for val in val_tuple:
-        res *= int(val)
-    return res
-
-
 sum_mul: int = 0
-MUL_RE = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
-DO_RE = re.compile(r"do\(\)")
-DONT_RE = re.compile(r"don't?\(\)")
+INSTR_RE = re.compile(r"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))")
 mul_enabled: bool = True
 for line in puzzle_input.splitlines():
-    mul_matches = MUL_RE.finditer(line)
-    do_matches = DO_RE.finditer(line)
-    do_inst: list[int] = [0, *[idx.start() for idx in do_matches]]
-    dont_matches = DONT_RE.finditer(line)
-    dont_inst: list[int] = [idx.start() for idx in dont_matches]
-    for mul_inst in mul_matches:
-        if do_inst[0] < mul_inst.start() < dont_inst[0]:
-            sum_mul += prod(mul_inst.groups())
-        else:
-            if do_inst[1] > mul_inst.start():
-                _ = do_inst.pop(0)
-            if dont_inst[0] < do_inst[0]:
-                _ = dont_inst.pop(0)
-
+    for instr in INSTR_RE.findall(line):
+        if instr[0] == "do()":
+            mul_enabled = True
+        elif instr[0] == "don't()":
+            mul_enabled = False
+        elif mul_enabled and instr[0].startswith("mul"):
+            sum_mul += prod(instr[1:])
 
 print(f"Sum of all valid mul instructions: {sum_mul}")
